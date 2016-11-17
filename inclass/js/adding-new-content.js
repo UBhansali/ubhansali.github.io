0bcf6efb89377f6d9d006dadc56c7241c12e6eca
Udit Bhansali <udit@ymail.com>
2016-11-16 23:09:26 -0500

    Delete adding-new-content.js

--- inclass/js/adding-new-content.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('ul').before("<p>Just Updated</p>");
-    $('li.hot').prepend("+ ");
-    var $newListItem = $('<li><em>gluten-free </em>soy sauce</li>');
-    $('li:last').after($newListItem);
-    
-});
